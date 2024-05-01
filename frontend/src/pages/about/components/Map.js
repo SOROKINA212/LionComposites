cfa4f5e865f1a3bab4e6ec22e00c5e1aaaae6620
Sorokina Lera <sorokina.valer1a@yandex.ru>
2024-05-01 21:11:46 +0300

    Map
    Add coordinates and geotag on map

--- frontend/src/pages/about/components/Map.js
+++ frontend/src/pages/about/components/Map.js
@@ -2,16 +2,11 @@ import React from "react";
 import './Map.css';
 
 const MapComponent = () => {
-  return (
-    <div style={{ width: "604px", height: "450px", borderRadius: "45px", overflow: "hidden" }}>
-      <iframe
-        src="https://yandex.ru/map-widget/v1/?um=constructor%3A708f2da14b7c06ea6e59eddb02a9c4e54b9627268b3b8e5fbd0b1ff3c96326b0&amp;source=constructor"
-        width="100%"
-        height="100%"
-        frameBorder="0"
-      />
-    </div>
-  );
+    return (
+        <div style={{ width: "604px", height: "450px", borderRadius: "45px", overflow: "hidden" }}>
+            <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A708f2da14b7c06ea6e59eddb02a9c4e54b9627268b3b8e5fbd0b1ff3c96326b0&amp;source=constructor&amp;ll=39.686249%2C47.279403&amp;z=16&amp;pt=39.686249%2C47.279403" width="100%" height="100%" frameBorder="0" />
+        </div>
+    );
 };
 
 export default MapComponent;
